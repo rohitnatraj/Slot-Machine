8c659a2c78c78672d85834da0a82b3a0c1da5599
Rohit Natraj <rohitnatraj@gmail.com>
2021-11-10 13:39:20 -0700

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Natraj, Rohit on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
